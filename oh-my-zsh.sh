19dc5cd927decb889a36cb49725e1db839833276
Li Tong <diywang@gmail.com>
2020-11-19 17:05:05 +0800

    Create oh-my-zsh.sh

---
+++ oh-my-zsh.sh
@@ -0,0 +1,16 @@
+# !/bin/sh
+echo Installing oh-my-zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+
+echo Installing zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo Installing zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo Configuring .zshrc
+sed -E -i 's/^plugins=\((.*)\)/plugins=\(\1 zsh-autosuggestions zsh-syntax-highlighting\)/g' ${HOME}/.zshrc
+source ${HOME}/.zshrc
+
+echo All complete
