29e307f095b94ae69453d64ecf9e3adeec4c56ed
King-Hell <diywang@gmail.com>
2020-12-10 11:28:22 +0800

    Install YouCompleteMe

--- spf13-vim.sh
+++ spf13-vim.sh
@@ -1,5 +1,9 @@
 # !/bin/sh
 echo Set vim plugins YouCompleteMe and python
 cp vim_bundles $HOME/.vimrc.before.local
+
 echo Installing spf13-vim
 sh spf13_install.sh
+
+echo Install YouCompleteMe
+python3 $HOME/.vim/bundle/YouCompleteMe/install.py
