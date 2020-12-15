2838fd62db3ca2d270cadd17cdf3d6b522b9fc96
Tong Li <diywang@gmail.com>
2020-12-15 10:36:49 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,4 @@
 # !/bin/sh
-if [ -d ${HOME}/.ssh ];then
-    mkdir ${HOME}/.ssh
-fi
+mkdir -p ${HOME}/.ssh
 echo Import SSH keys
 export ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEr4wlocOhqckJjCTYFo1LPdhmjMwaZWI5mK+pehhB5VcLqUJ4mIPOH/sMVU0LfaK7JoHc/M0fo2Cjqc9tQXdbf1+/lbRabILgX4pdH+eAaou+8qFOAAys5uLy36SelcIZMAeei3yg+jGvqjPbNSd1TcBId8DWsfjyi9kJTVL2gjYix49J/7Yx8nAgIJ68J88tmQ1+rSSP/GWNe9cjm3zu9E1fxe/vIOtzYJHZBVdxC8PGFrp1Ra/ltRZrj7zsxIm6NuiJPWXt1z7rzy9Da9zTzylhUGrfbFxW4VAlH/MjDXW1xBHO3Q9xkKmaCL9XYaFxcJREybk7Ns7o7JWe2WSOZ8VrvWHjNyjWY9kKQb+jyTPQ5pkoxRcAIrux6gRfFOHdgRRwMB0lQHPFifrqYkUI7ixPptQGn/p3yfFKM31za7bWmHOfNdQkecOsaj8r2QH4O3vrBJ6QTAriPOrE/UMlvXtN5gwOU/C7lePnXUfUOfBHYcd/9aOIhVYymCt9N9s= diywa@DESKTOP-VKVPR2A>>${HOME}/.ssh/authorized_keys
