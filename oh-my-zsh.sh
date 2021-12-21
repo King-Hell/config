841f16d1decff16e5b17a209cf7df5b80bfebfc1
Tong Li <diywang@gmail.com>
2021-12-21 16:25:46 +0800

    change https to git

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -4,10 +4,10 @@ echo Installing oh-my-zsh
 sh omz_install.sh
 
 echo Installing zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo Installing zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo Configuring .zshrc
 sed -E -i 's/^plugins=\((.*)\)/plugins=\(\1 extract zsh-autosuggestions zsh-syntax-highlighting\)/g' ${HOME}/.zshrc
